fa217ec73412d27c30a03e263315f777c046e8f3
Kuzin Ilya <kuzin.ilya@gmail.com>
2024-12-21 05:14:56 +0700

    efrfrfr

---
+++ l3/tsu_module.c
@@ -0,0 +1,23 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Your Name");
+MODULE_DESCRIPTION("TSU kernel module");
+
+// Функция инициализации
+static int __init tsu_init(void)
+{
+    printk(KERN_INFO "Welcome to the Tomsk State University\n");
+    return 0;
+}
+
+// Функция выгрузки
+static void __exit tsu_exit(void)
+{
+    printk(KERN_INFO "Tomsk State University forever!\n");
+}
+
+module_init(tsu_init);
+module_exit(tsu_exit);
